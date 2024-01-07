84d7ba5eca67cc4557faed403a65a0a4a848fcdf
AleksandraMN <al_mn@mail.ru>
2024-01-07 13:37:57 +1000

    Lesson4_2.2

--- tasksHard_1.js
+++ tasksHard_1.js
@@ -8,13 +8,16 @@ let sumTax = 0;
 
 if (salary <= 0 || Number.isNaN(salary)) {
    console.log("Вы ввели некорректные данные!");
-} else if (salary < 15000) {
+}  
+if (salary < 15000) {
    sumTax = salary * 0.13;
    console.log(`Сумма налога равна ${sumTax} рублей.`);
-} else if (salary >= 15000 && salary <= 50000) {
+}  
+if (salary >= 15000 && salary <= 50000) {
    sumTax = salary * 0.20;
    console.log(`Сумма налога равна ${sumTax} рублей.`);
-} else if (salary > 50000) {
+} 
+if (salary > 50000) {
    sumTax = salary * 0.30;
    console.log(`Сумма налога равна ${sumTax} рублей.`);
 };
@@ -23,22 +26,22 @@ if (salary <= 0 || Number.isNaN(salary)) {
 
 const salaryPro = Number(prompt("Введите цифрами сумму заработка?", ""));
 
-let sumTax1 = 0;
-let sumTax2 = 0;
+const sumTax1 = 15000 * 0.13;
+const sumTax2 = 35000 * 0.20;
 let sumTax3 = 0;
 
 if (salaryPro <= 0 || Number.isNaN(salaryPro)) {
    console.log("Вы ввели некорректные данные!");
-} else if (salaryPro <= 15000) {
-   sumTax1 = salaryPro * 0.13;
-   console.log(`Сумма налога равна ${sumTax1} рублей.`);
-} else if (salaryPro > 15000 && salaryPro <= 50000) {
-   sumTax1 = 15000 * 0.13;
-   sumTax2 = (salaryPro - 15000) * 0.20;
-   console.log(`Сумма налога равна ${sumTax1 + sumTax2} рублей.`);
-} else if (salaryPro > 50000) {
-   sumTax1 = 15000 * 0.13;
-   sumTax2 = 35000 * 0.20;
+}  
+if (salaryPro <= 15000) {
+   sumTax3 = salaryPro * 0.13;
+   console.log(`Сумма налога равна ${sumTax3} рублей.`);
+} 
+if (salaryPro > 15000 && salaryPro <= 50000) {
+   sumTax3 = (salaryPro - 15000) * 0.20;
+   console.log(`Сумма налога равна ${sumTax1 + sumTax3} рублей.`);
+} 
+if (salaryPro > 50000) {
    sumTax3 = (salaryPro - 50000) * 0.30;
    console.log(`Сумма налога равна ${sumTax1 + sumTax2 + sumTax3} рублей.`);
 };
